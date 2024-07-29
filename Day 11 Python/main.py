64456af3ff7b11f5f2d4f3cdb7b91999d4401935
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2024-07-29 05:52:59 +0000

    added functions and also converted to objects and methods

--- Day 11 Python/main.py
+++ Day 11 Python/main.py
@@ -1,6 +1,8 @@
 def salary(hours_worked, hour_rate):
     return hours_worked * hour_rate
+
 employee_list = []
+
 def add_employee(id, name):
     new_employee = {'id': id, 'name': name}
     employee_list.append(new_employee)
@@ -12,7 +14,6 @@ print(add_employee(13, "rohit"))
 print(employee_list)
 
 class Employee:
-
     def __init__(self, name, hours_worked, hour_rate):
         self.name = name
         self.hours_worked = hours_worked
@@ -20,6 +21,10 @@ class Employee:
     
     def salary(self):
         return self.hours_worked * self.hour_rate
+    
+    def __str__(self):
+        return f"Employee Name: {self.name}, Hours Worked: {self.hours_worked}, Hour Rate: {self.hour_rate}, Salary: {self.salary()}"
+
 class Employer:
     def __init__(self):
         self.employees = []
@@ -30,7 +35,7 @@ class Employer:
     def calculate_total_pay(self):
         total_pay = 0
         for employee in self.employees:
-            total_pay+= employee.salary()
+            total_pay += employee.salary()
         return total_pay
 
     def display_all(self):
@@ -38,12 +43,14 @@ class Employer:
             print(employee)
 
 employer = Employer()
-emp1 = Employee("lokesh",10,20)
-emp2 = Employee("ram",16, 25)
+emp1 = Employee("Lokesh", 10, 20)
+emp2 = Employee("Ram", 16, 25)
 employer.add_employee(emp1)
 employer.add_employee(emp2)
 
-print(employer.calculate_total_pay())
+print("Total Pay:", employer.calculate_total_pay())
+print("Employee Details:")
 employer.display_all()
+
 
 
